9d6808d974045c8609cfe77678e16e2a5d709fd0
helloeka <helloeka@gmail.com>
2020-01-22 15:19:44 +0700

    update eka

---
+++ modal-login.php
@@ -0,0 +1,39 @@
+<div class="modal-header top-header-custom">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          </div>
+          <div class="modal-body body-modal-custom">
+            <form class="form-horizontal">
+              <div class="form-group">
+                <div class="col-sm-12">
+                  <input type="email" class="form-control form-control-custom" id="inputEmail3" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-12">
+                  <input type="password" class="form-control form-control-custom" id="inputPassword3" placeholder="Password">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-12">
+                  <div class="forgot-password">
+                    <label>
+                      <a href="#">Forgot Password ?</a> 
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-12">
+                  <button type="submit" class="btn btn-default btn-lg btn-block btn-login">Sign in</button>
+                  <center>
+                    <label class="dont-yet">
+                    Don't have an account yet? <a href="#">Sign Up!</a> 
+                    </label>
+                  </center>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer bottom-header-custom">
+           
+          </div>
